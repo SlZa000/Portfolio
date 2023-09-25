d8a0950a84561dca1599f40a9ccf517e98e2e0eb
Sławomir Zając <87309470+Mirac00@users.noreply.github.com>
2023-09-25 17:43:29 +0200

    slider fixed

--- src/components/slider.js
+++ src/components/slider.js
@@ -4,51 +4,66 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './slider.css';
 
-const PhotoSlider = ({ images }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-    centerMode: true,
-    centerPadding: '0',
-    nextArrow: <SamplePrevArrow />, // Zamienione
-    prevArrow: <SampleNextArrow />, // Zamienione
-  };
+class PhotoSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.slider = React.createRef();
+  }
 
-  return (
-    <div className='slider-box'>
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt='image' className='image' />
-          </div>
-        ))}
-      </Slider>
-    </div>
-  );
-};
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      centerMode: true,
+      centerPadding: '0',
+      nextArrow: <SamplePrevArrow sliderRef={this.slider} />, // Przekazanie referencji do strzałek
+      prevArrow: <SampleNextArrow sliderRef={this.slider} />, // Przekazanie referencji do strzałek
+    };
+
+    return (
+      <div className='slider-box'>
+        <Slider ref={this.slider} {...settings}>
+          {this.props.images.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt='image' className='image' />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
+}
 
 function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
+  const { className, style, onClick, sliderRef } = props;
   return (
     <div
       className={className}
       style={{ ...style, display: "block", background: "white" }}
-      onClick={onClick}
+      onClick={() => {
+        if (sliderRef.current) {
+          sliderRef.current.slickPrev(); // Odwrócenie logiki przewijania
+        }
+      }}
     />
   );
 }
 
 function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
+  const { className, style, onClick, sliderRef } = props;
   return (
     <div
       className={className}
       style={{ ...style, display: "block", background: "white" }}
-      onClick={onClick}
+      onClick={() => {
+        if (sliderRef.current) {
+          sliderRef.current.slickNext(); // Odwrócenie logiki przewijania
+        }
+      }}
     />
   );
 }
