20c6bd2665188df067cece5b6d3c7213e5487ad7
Sławomir Zając (Mirac00) <slawomir.zajac@example.com>
2024-09-01 21:40:30 +0200

    buildv3

--- src/components/pages/interview.js
+++ src/components/pages/interview.js
@@ -15,9 +15,9 @@ export default function Interview({ profileImage, scrollToAboutMe }) {
       <div className="interview-text-container">
         <p className="interview-text">
           <h1>Sławomir Zając</h1><h2>Fullstack Developer</h2><br/>
-          Cześć! Nazywam się Sławomir Zając. Jestem na ostatnim roku Informatyki Stosowanej i niedawno ukończyłem praktyki, podczas których zdobyłem cenne doświadczenie w pracy nad rzeczywistymi projektami. Pasjonuje mnie tworzenie innowacyjnych rozwiązań, które mogą realnie wpływać na świat.
+          Cześć! Nazywam się Sławomir Zając. Jestem na ostatnim roku Informatyki Stosowanej i niedawno ukończyłem praktyki. Obecnie jestem gotowy na stałą współpracę przy poważnych projektach. Moją pasją jest frontend, ale mam też doświadczenie w backendzie, co pozwala mi tworzyć spójne i funkcjonalne aplikacje.
 
-Na mojej stronie znajdziesz nie tylko moje CV, ale także szczegółowe opisy projektów, nad którymi pracowałem. Każdy z nich pokazuje moje podejście do programowania, kreatywność oraz umiejętność rozwiązywania problemów. Zachęcam Cię do zapoznania się z moim portfolio i odkrycia, jak mogę pomóc w Twoich projektach.
+Na mojej stronie znajdziesz moje CV oraz szczegóły projektów, nad którymi pracowałem. Zachęcam do zapoznania się z moim portfolio i odkrycia, jak mogę pomóc w realizacji Twoich projektów!
 
 Chcesz dowiedzieć się więcej? Kliknij poniżej, aby zobaczyć moje CV i pełne portfolio!
           <br/>
